819cfec33ea8e8b11bed8d7d013af19ad72f7b71
Christofer Krabbi <christoferkrabbi@gmail.com>
2025-11-17 12:08:30 +0200

    2

--- content/gitiKasutamine.php
+++ content/gitiKasutamine.php
@@ -1,11 +1,15 @@
 <?php
 echo "<h2>GIT CMD käsud</h2>";
-echo "<ul>";
+echo "<ol>";
 echo "<li>git init - uus repo </li>";
 ?>
 <li>
     Nimi ja konto esimend config
+    <pre>
+        git config --global user.name "nimi perenimi"
+        git config --global user.email "nimi perenimi"
+        git config --global --list
+    </pre>
 </li>
-<pre>
-    git
-</pre>
+
+</ol>
