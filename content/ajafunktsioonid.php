ad8459d842c1e317649237b473de14728e4661e0
christoferkrabbi <christoferkrabbi@gmail.com>
2025-11-19 18:07:39 +0200

    Div flex for ajafunktsioonid

--- content/ajafunktsioonid.php
+++ content/ajafunktsioonid.php
@@ -1,4 +1,7 @@
 <?php
+
+echo "<div class='flex-container'>";
+echo "<div>";
 echo "<h1>Ajafunktsioonid</h1>";
 echo "tänane kuupäev ".date("d.m.Y");
 //timezone - juhul kui timezone ei ole määratud,
@@ -19,9 +22,41 @@ G - 24h formaat
 i - minutid 0...59
 s -sekundid 0...59
 </pre>";
+echo "</div>";
+
+echo "<div>";
+echo "<h1>Tehtud kuupäevaga</h1>";
 echo "<br>";
 echo "+1min = time()+60 = ".date('d.m.Y G:i:s', time() +60);
 echo "<br>";
 echo "+1tund = time()+60*60 = ".date('d.m.Y G:i:s', time() +60*60);
 echo "<br>";
 echo "+1päev = time()+60*60*24 = ".date('d.m.Y G:i:s', time() +60*60*24);
+echo "<br>";
+echo "mktime(tunnid, minutid, sekundid, kuu, päev, aasta)";
+echo "<br>";
+echo "</div>";
+
+echo "<div>";
+echo "<h1>Kindlad kuupäevad</h1>";
+$synnipaev=mktime(10, 0, 0, 1, 23, 2007);
+echo "Minu sünnipäev: ".date("d.m.Y G:i:s", $synnipaev);
+echo "<br>";
+echo "massiivi (array) abil näidata tänane kuu";
+echo "<br>";
+$kuud=array(1=>'jaanuar','veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+$paev=date('d');
+$aasta=date('Y');
+$kuu=$kuud[date('m')]; //kuu nimega
+echo "tänase kuupäev kuu nimega: ".$paev.". ".$kuu." ".$aasta.".a";
+echo "<br>";
+$synnipaevpaev=date('23');
+$synniaasta=date('2007');
+$kuu=$kuud[date('1')]; //kuu nimega
+echo "sünnipäeva kuupäev kuu nimega: ".$synnipaevpaev.". ".$kuu." ".$synniaasta.".a";
+echo "</div>";
+echo "</div class='flex-container'>";
+
+
+
+
